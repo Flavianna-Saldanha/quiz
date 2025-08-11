501bd8686c38b02306953ce40e70d6c7a28b4440
Flavianna Saldanha <flaviannasaldanha@gmail.com>
2025-08-11 11:28:07 -0300

    finalizado!

---
+++ src/data/questions.ts
@@ -0,0 +1,104 @@
+import { Question } from "@/types/Question";
+
+export const questions: Question[] = [
+    {
+        question: 'Qual é a capital da Austrália?',
+        options: [
+            'Sydney',
+            'Melbourne',
+            'Canberra',
+            'Brisbane'
+        ],
+        answer: 2
+    },
+    {
+        question: 'Quem pintou a Mona Lisa?',
+        options: [
+            'Michelangelo',
+            'Leonardo da Vinci',
+            'Rafael',
+            'Donatello'
+        ],
+        answer: 1
+    },
+    {
+        question: 'Qual é o maior planeta do Sistema Solar?',
+        options: [
+            'Saturno',
+            'Marte',
+            'Júpiter',
+            'Netuno'
+        ],
+        answer: 2
+    },
+    {
+        question: 'Qual elemento químico tem o símbolo "O"?',
+        options: [
+            'Ouro',
+            'Oxigênio',
+            'Ósmio',
+            'Prata'
+        ],
+        answer: 1
+    },
+    {
+        question: 'Qual país é conhecido como a Terra do Sol Nascente?',
+        options: [
+            'China',
+            'Japão',
+            'Coreia do Sul',
+            'Vietnã'
+        ],
+        answer: 1
+    },
+    {
+        question: 'Quem desenvolveu a teoria da relatividade?',
+        options: [
+            'Isaac Newton',
+            'Galileo Galilei',
+            'Nikola Tesla',
+            'Albert Einstein'
+        ],
+        answer: 3
+    },
+    {
+        question: 'Qual é o maior oceano do planeta Terra?',
+        options: [
+            'Oceano Atlântico',
+            'Oceano Índico',
+            'Oceano Pacífico',
+            'Oceano Ártico'
+        ],
+        answer: 2
+    },
+    {
+        question: 'Em que ano o homem pisou na Lua pela primeira vez?',
+        options: [
+            '1959',
+            '1969',
+            '1979',
+            '1989'
+        ],
+        answer: 1
+    },
+    {
+        question: 'Qual é o idioma oficial do Egito?',
+        options: [
+            'Inglês',
+            'Árabe',
+            'Francês',
+            'Espanhol'
+        ],
+        answer: 1
+    },
+    {
+        question: 'Quem foi o primeiro presidente dos Estados Unidos?',
+        options: [
+            'Thomas Jefferson',
+            'Abraham Lincoln',
+            'George Washington',
+            'John Adams'
+        ],
+        answer: 2
+    }
+];
